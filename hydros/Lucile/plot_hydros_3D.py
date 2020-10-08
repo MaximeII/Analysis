d92e7c29a35b3a1ca009dde56474925b479e5900
Maxime Dubart <maxime.dubart@helsinki.fi>
2020-10-08 11:54:33 +0300

    Added growth rate colorbar

--- hydros/Lucile/plot_hydros_3D.py
+++ hydros/Lucile/plot_hydros_3D.py
@@ -7,8 +7,9 @@ from mpl_toolkits.mplot3d import Axes3D
 theta     = np.arange(40.0,90.1,0.1)
 k         = np.arange(0.00,5.01,0.01)
 omegaFULL = np.empty([len(k),len(theta)])
+gammaFULL = np.empty([len(k),len(theta)])
 
-w_start = 0.01
+w_start = 1.0
 
 path_output = '/wrk/users/dubart/analysis/hydros/Lucile/output/'
 path_fig    = '/wrk/users/dubart/analysis/hydros/Lucile/fig/'
@@ -27,20 +28,32 @@ for i in range(0,len(theta)):
    
     omega    = np.empty(len(k))
     omega[:] = np.NAN   
+    
+    gamma    = np.empty(len(k))
+    gamma[:] = np.NAN
 
     if len(HYDROS.shape) == 2:
         omegaHY = HYDROS[:,2]
         omega[:len(omegaHY)] = omegaHY
+        gammaHY = -HYDROS[:,3]
+        gamma[:len(gammaHY)] = gammaHY
     else:
-        omegaHY = HYDROS[2]
+        omegaHY  = HYDROS[2]
         omega[0] = omegaHY
+        gammaHY  = -HYDROS[3]
+        gamma[0] = gammaHY
 
+    gammaFULL[:,i] = gamma
     omegaFULL[:,i] = omega
 
 masked_omega=np.ma.masked_invalid(omegaFULL)
+masked_gamma=np.ma.masked_invalid(gammaFULL)
 
 w_min = np.amin(masked_omega)
 w_max = 2.0
+
+gamma_min = 0.0
+gamma_max = 0.5
 
 for i in range(0,len(theta)):
 
@@ -53,14 +66,20 @@ for i in range(0,len(theta)):
     if len(HYDROS.shape) == 2:
         omegaHY = HYDROS[:,2]
         omega[:len(omegaHY)] = omegaHY
+        gammaHY = -HYDROS[:,3]
+        gamma[:len(gammaHY)] = gammaHY
     else:
-        omegaHY = HYDROS[2]
+        omegaHY  = HYDROS[2]
         omega[0] = omegaHY
-
-    p=ax.scatter3D(k,theta[i],omega,c=omega,cmap='viridis',vmin=w_min,vmax=w_max)
+        gammaHY  = -HYDROS[3]
+        gamma[0] = gammaHY
+    
+    p=ax.scatter3D(k,theta[i],omega,c=gamma,cmap='viridis',vmin=gamma_min,vmax=gamma_max)
 
 cbar=fig.colorbar(p)
-cbar.mappable.set_clim(0.0,2.0)
+#cbar.mappable.set_clim(0.0,2.0)
+cbar.set_label('$\\gamma/\\Omega_C$')
+
 
 plt.savefig(path_fig+'Lucile_3D_'+str(w_start)+'.png',DPI=300)  
 print(path_fig+'Lucile_3D_'+str(w_start)+'.png')
